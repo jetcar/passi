497be071b3edd531626810657e8271a849654ca1
vova <jetcarq@msn.com>
2022-11-15 20:45:53 +0200

    build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+cd /home/passi
+
+
+
+dotnet publish SampleApp/WebApp/WebApp.csproj -c Release -o SampleApp/WebApp/publish
+dotnet publish passiwebapi/passiwebapi.csproj -c Release -o passiwebapi/publish
+dotnet publish IdentityServer/IdentityServer.csproj -c Release -o IdentityServer/publish
+

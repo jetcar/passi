24d981b8ab146df8eb6ef705a02aeed7b7e1dcaa
vova <jetcarq@msn.com>
2022-12-14 21:46:11 +0200

    fix build

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ git checkout .
 git fetch
 git pull
 
-dotnet publish SampleApp/WebApp/WebApp.csproj -c Release -o SampleApp/WebApp/publish
+dotnet publish WebApp/WebApp.csproj -c Release -o WebApp/publish
 dotnet publish passiwebapi/passiwebapi.csproj -c Release -o passiwebapi/publish
 dotnet publish IdentityServer/IdentityServer.csproj -c Release -o IdentityServer/publish
 

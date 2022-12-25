627197f6bb3cb383be8b1401386a24cbd3eadf64
vova <jetcarq@msn.com>
2022-12-25 12:21:57 +0200

    fix deploy

--- Services/FireBaseClient.cs
+++ Services/FireBaseClient.cs
@@ -14,9 +14,7 @@ namespace Services
     {
         public FireBaseClient()
         {
-            var filePath = "google-services.json";
-            if (!File.Exists(filePath))
-                filePath = "publish/google-services.json";
+            var filePath = "/home/creds/google-services.json";
             var credential = GoogleCredential.FromFile(filePath);
             FirebaseApp.Create(new AppOptions()
             {

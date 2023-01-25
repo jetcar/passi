dae37b1d7bff4a2e379d76aa41153631bc5f94e5
vova <jetcarq@gmail.com>
2023-01-25 22:13:28 +0200

    rename build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
-cd /home/passi
-
-
 
 dotnet publish SampleApp/WebApp/WebApp.csproj -c Release -o SampleApp/WebApp/publish
 dotnet publish passiwebapi/passiwebapi.csproj -c Release -o passiwebapi/publish

de11cb9c4c64034f64c4967a001f6e801fff5990
vova <jetcarq@gmail.com>
2023-01-28 22:59:29 +0200

    update .net core 7

---
+++ Identity/IdentityModel/Client/Messages/TokenRequest.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace IdentityModel.Client;
+
+/// <summary>
+/// Request for token
+/// </summary>
+/// <seealso cref="ProtocolRequest" />
+public class TokenRequest : ProtocolRequest
+{
+    /// <summary>
+    /// Gets or sets the type of the grant.
+    /// </summary>
+    /// <value>
+    /// The type of the grant.
+    /// </value>
+    public string GrantType { get; set; } = default!;
+}
---
+++ Identity/IdentityModel/Internal/InternalStringExtensions.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace IdentityModel.Internal;
+
+internal static class InternalStringExtensions
+{
+    [DebuggerStepThrough]
+    public static bool IsMissing(this string value)
+    {
+        return string.IsNullOrWhiteSpace(value);
+    }
+
+    [DebuggerStepThrough]
+    public static bool IsPresent(this string value)
+    {
+        return !(value.IsMissing());
+    }
+
+    [DebuggerStepThrough]
+    public static string EnsureTrailingSlash(this string url)
+    {
+        if (!url.EndsWith("/"))
+        {
+            return url + "/";
+        }
+
+        return url;
+    }
+
+    [DebuggerStepThrough]
+    public static string RemoveTrailingSlash(this string url)
+    {
+        if (url == null) throw new ArgumentNullException(nameof(url));
+        
+        if (url.EndsWith("/"))
+        {
+            url = url.Substring(0, url.Length - 1);
+        }
+
+        return url;
+    }
+}

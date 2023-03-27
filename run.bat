2efdc3238ea1d52ef64d63e017f119d0cb8d89d4
vova <jetcarq@gmail.com>
2023-03-27 21:26:45 +0300

    wireguard

--- run.bat
+++ run.bat
@@ -6,5 +6,9 @@ docker volume prune -f
 sudo docker pull mcr.microsoft.com/dotnet/sdk:7.0
 sudo docker pull mcr.microsoft.com/dotnet/aspnet:7.0
 
+docker build . -f WebApp/Dockerfile -t webapp:1.0.4
+docker build . -f IdentityServer/Dockerfile -t identityserver:1.0.4
+docker build . -f passiwebapi/Dockerfile -t passiwebapi:1.0.4
+
 docker-compose down
-docker-compose -f docker-compose.yml up --force-recreate --build
+docker-compose -f docker-compose.yml up  --force-recreate --build

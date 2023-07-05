74651b9e89c2fe63b0eb40f99ee9713a4af2aaee
vova <jetcarq@gmail.com>
2023-07-05 13:25:37 +0300

    clear docker on deploy

--- run.sh
+++ run.sh
@@ -4,8 +4,16 @@ git checkout .
 git fetch
 git pull
 
+sudo docker-compose down
+sudo docker stop $(docker ps -aq)
+sudo docker rm $(docker ps -aq)
+
 sudo docker image prune -f --all
 sudo docker volume prune -f --all
+sudo service docker stop
+sudo rm -rf /var/lib/docker/overlay2
+sudo service docker start
+
 
 sudo docker pull mcr.microsoft.com/dotnet/sdk:7.0
 sudo docker pull mcr.microsoft.com/dotnet/aspnet:7.0
@@ -15,5 +23,4 @@ sudo docker build . -f IdentityServer/Dockerfile -t identityserver:1.0.4
 sudo docker build . -f passiwebapi/Dockerfile -t passiwebapi:1.0.4
 
 
-sudo docker-compose down
 sudo docker-compose -f docker-compose.yml up

53095aba684c8da1d5ede3abff92770a943cd945
vova <jetcarq@gmail.com>
2023-08-10 21:50:42 +0300

    postgres update preparations

--- run.sh
+++ run.sh
@@ -6,14 +6,16 @@ git checkout .
 git fetch
 git pull
 
+sudo docker compose down
+sudo docker system prune -a -f
+sudo docker image prune -f --all
+sudo docker volume prune -f --all
+
+
 sudo docker pull mcr.microsoft.com/dotnet/sdk:7.0
 sudo docker pull mcr.microsoft.com/dotnet/aspnet:7.0
 
 sudo docker compose -f docker-compose.yml build
-sudo docker compose down
-docker system prune -a -f
-sudo docker image prune -f --all
-sudo docker volume prune -f --all
 
 
 sudo docker compose -f docker-compose.yml up

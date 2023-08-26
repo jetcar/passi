5a515e92bbfd431cdf4664f375f940fdef32a363
vova <jetcarq@gmail.com>
2023-08-26 09:11:27 +0300

    fix run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,20 @@ sudo git clean -fdx
 git checkout .
 git fetch
 git pull
-sudo ./run_deploy.sh
+#sudo ./run_deploy.sh
+
+sudo docker compose down
+sudo docker exec -it postgres pg_ctl stop -m smart
+sudo docker stop $(docker ps -a -q)
+sudo docker system prune -a -f
+sudo docker image prune -f --all
+sudo docker volume prune -f --all
+
+
+sudo docker pull mcr.microsoft.com/dotnet/sdk:7.0
+sudo docker pull mcr.microsoft.com/dotnet/aspnet:7.0
+
+sudo docker compose -f docker-compose.yml build
+
+sudo docker compose -f docker-compose.yml up
 

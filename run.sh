22dcf7c381b2d5d5850f652c12e01f8234731ee7
vova <jetcarq@gmail.com>
2023-08-31 21:03:42 +0300

    refactoring

--- run.sh
+++ run.sh
@@ -7,18 +7,13 @@ git fetch
 git pull
 #sudo ./run_deploy.sh
 
-sudo docker compose down
 sudo docker exec -it postgres pg_ctl stop -m smart
+sudo docker compose down
 sudo docker stop $(docker ps -a -q)
 sudo docker system prune -a -f
 sudo docker image prune -f --all
 sudo docker volume prune -f --all
 
-
-sudo docker pull mcr.microsoft.com/dotnet/sdk:7.0
-sudo docker pull mcr.microsoft.com/dotnet/aspnet:7.0
-
 sudo docker compose -f docker-compose-cloud.yml build
-
 sudo docker compose -f docker-compose-cloud.yml up
 

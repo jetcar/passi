2b482d1a9ed16a5846cc0dbe13b81e6096c5a72b
jetcar <jetcarq@gmail.com>
2023-12-07 21:18:21 +0200

    webapp ui

--- build dockers.bat
+++ build dockers.bat
@@ -1,4 +1,3 @@
-docker build . -f WebAppUi/Dockerfile -t webappui:1.0.11 -t webappui:latest
 docker build . -f Dockerfile -t common_image:1.0.11 -t common_image:latest
 docker build . -f identityserver/Dockerfile -t jetcar/identityserver:1.0.11
 docker build . -f webapp/Dockerfile -t jetcar/webapp:1.0.11

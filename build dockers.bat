339b63eb038d80428a5ca181f599aa0b51c92532
jetcar <jetcarq@gmail.com>
2023-12-11 20:12:18 +0200

    1.0.12

--- build dockers.bat
+++ build dockers.bat
@@ -1,6 +1,6 @@
-docker build . -f Dockerfile -t common_image:1.0.11 -t common_image:latest
-docker build . -f identityserver/Dockerfile -t jetcar/identityserver:1.0.11
-docker build . -f webapp/Dockerfile -t jetcar/webapp:1.0.11
-docker build . -f passiwebapi/Dockerfile -t jetcar/passiwebapi:1.0.11
+docker build . -f Dockerfile -t common_image:1.0.12 -t common_image:latest
+docker build . -f identityserver/Dockerfile -t jetcar/identityserver:1.0.12
+docker build . -f webapp/Dockerfile -t jetcar/webapp:1.0.12
+docker build . -f passiwebapi/Dockerfile -t jetcar/passiwebapi:1.0.12
 
 pause

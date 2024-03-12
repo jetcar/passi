efe344a70789020ccdff4550637f95c5fa5832a0
jetcar <jetcarq@gmail.com>
2024-03-12 20:31:44 +0200

    1.0.27

--- build dockers push.bat
+++ build dockers push.bat
@@ -1,5 +1,5 @@
 
-docker push jetcar/identityserver:1.0.26
-docker push jetcar/webapp:1.0.26
-docker push jetcar/passiwebapi:1.0.26
+docker push jetcar/identityserver:1.0.27
+docker push jetcar/webapp:1.0.27
+docker push jetcar/passiwebapi:1.0.27
 pause

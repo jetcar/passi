46159f4986fab7cc8e2104ad0da28c49f98e377e
jetcar <jetcarq@gmail.com>
2024-07-06 22:39:49 +0300

    update libraries

--- build dockers push.bat
+++ build dockers push.bat
@@ -1,5 +1,5 @@
 
-docker push jetcar/identityserver:1.0.33
-docker push jetcar/webapp:1.0.33
-docker push jetcar/passiwebapi:1.0.33
+docker push jetcar/identityserver:1.0.34
+docker push jetcar/webapp:1.0.34
+docker push jetcar/passiwebapi:1.0.34
 pause

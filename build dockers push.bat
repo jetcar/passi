39a26eaa6e9f7a8be14efd456bc32855c7251e51
jetcar <jetcarq@gmail.com>
2025-03-06 21:30:52 +0200

    openidc works using vue, with cert validation

--- build dockers push.bat
+++ build dockers push.bat
@@ -1,6 +1,6 @@
 
-docker push jetcar/identityserver:1.0.40
-docker push jetcar/webapp:1.0.40
-docker push jetcar/passiwebapi:1.0.40
-docker push jetcar/openidc:1.0.40
+docker push jetcar/identityserver:1.0.41
+docker push jetcar/webapp:1.0.41
+docker push jetcar/passiwebapi:1.0.41
+docker push jetcar/openidc:1.0.41
 pause

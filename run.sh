35c89e6533ba118fb860ff89016df06164845075
jetcar <jetcarq@gmail.com>
2025-04-01 21:28:39 +0300

    docker recreate

--- run.sh
+++ run.sh
@@ -6,12 +6,12 @@ git checkout .
 git fetch
 git pull
 #sudo ./run_deploy.sh
-sudo docker stop $(docker ps -q)
-sudo docker rm $(docker ps -a -q)
+sudo docker stop $(sudo docker ps -q)
+sudo docker rm $(sudo docker ps -a -q)
 
 sudo docker compose -f docker-compose.yml build
 sudo docker compose -f docker-compose.yml up -d --remove-orphans
-cd run.sh ../run.sh
+
 #sudo docker push jetcar/passiwebapi:1.0.10
 #sudo docker push jetcar/identityserver:1.0.10
 #sudo docker push jetcar/webapp:1.0.10

40b030823acb7ced0b80841a142c47c4764533e0
jetcar <jetcarq@gmail.com>
2025-04-01 21:26:45 +0300

    porkbun-ddns

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ sudo docker rm $(docker ps -a -q)
 
 sudo docker compose -f docker-compose.yml build
 sudo docker compose -f docker-compose.yml up -d --remove-orphans
-
+cd run.sh ../run.sh
 #sudo docker push jetcar/passiwebapi:1.0.10
 #sudo docker push jetcar/identityserver:1.0.10
 #sudo docker push jetcar/webapp:1.0.10
